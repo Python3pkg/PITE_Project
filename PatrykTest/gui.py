4327ef4118a1bcec8f80392bc146a0a130c30c9d
PatrykPasterny <patryk.pasterny@gmail.com>
2016-12-29 23:24:11 +0100

    version v.1.1

--- PatrykTest/gui.py
+++ PatrykTest/gui.py
@@ -17,14 +17,19 @@ class GUI(Frame):
         self.filelist=[]
         self.file=" "
         self.folder="."
+        self.canvas=[]
+        self.TOOLBAR=[]
         ############################## 
         
         self.win = parent
         self.win.geometry("1600x1200")
-        self.win.title("GUI-V1")
+        self.win.title("GUI-V2")
         #MENU
         self.menu=Menu(self.win)
         self.menu.add_command(label="EXIT",command=self.EXIT)
+
+
+
         #FRAMES
         self.f1 = Frame(self.win)  # TREE
         self.f1_B = Frame(self.f1)  # buttons
@@ -33,22 +38,20 @@ class GUI(Frame):
         self.f2 = Frame(self.win)  # opcje w przyszlosci
         self.f2_B = Frame(self.f2)  # buttons
          
-        self.f3 = Frame(self.win,height=300, width=1500)
-        self.f3_1=0 # tak ma byc
 
         
 
-        # BUTTONSi
-        self.b1 = Button(self.win, text="EXIT",bg="red", fg="white")  # EXIT
+        # BUTTONS
+        self.TREE=[]   # FIND FILE
+        self.TREE.append(Button(self.f1_B, text="REFRESH"))
+        self.TREE.append(Button(self.f1_B, text="LOAD"))
+        self.TREE.append(Button(self.f1_B, text="FOLDER"))
 
-        self.b2 = Button(self.f1_B, text="REFRESH")  # TREE
-        self.b3 = Button(self.f1_B, text="LOAD")
-        self.b7 = Button(self.f1_B, text="FOLDER")
-
-        self.b4 = Button(self.f2_B, text="B 1")  # CANVAS
-        self.b5 = Button(self.f2_B, text="B 2")
-        self.b6 = Button(self.f2_B, text="B 3")
-        self.b8= Button(self.f2_B,text="B 4")
+        self.BUTTONS=[]
+        self.BUTTONS.append( Button(self.f2_B, text="B 1") )
+        self.BUTTONS.append( Button(self.f2_B, text="B 2") )
+        self.BUTTONS.append( Button(self.f2_B, text="B 3") )
+        self.BUTTONS.append( Button(self.f2_B, text="B 4") )
         # LABELS
         self.l1=Label(self.win,text="START")
 
@@ -65,13 +68,10 @@ class GUI(Frame):
 
     
         #PACKING
-        self.b2.pack(side=LEFT)
-        self.b3.pack(side=LEFT)
-        self.b4.pack(side=LEFT)
-        self.b5.pack(side=LEFT)
-        self.b6.pack(side=LEFT)
-        self.b7.pack(side=LEFT) 
-        self.b8.pack(side=LEFT)
+	for i in self.TREE:
+           i.pack(side=LEFT)
+	for i in self.BUTTONS:
+           i.pack(side=LEFT)
 
         self.sb1.pack(side=LEFT,fill=Y)
         
@@ -80,44 +80,29 @@ class GUI(Frame):
         
         self.f1_B.pack(side=TOP)
         self.f1_L.pack(side=TOP)
+
         self.f2_B.grid(row=0,column=0)
-
-        
-
-        #GRID SETUP
-        #self.b1.grid(row=3, column=2)
         self.l1.grid(row=1,column=2)
-        self.f1.grid(row=5,column=2)
+        self.f1.grid(row=5,column=2,sticky= S)
         self.f2.grid(row=1, column= 0)
-        self.f3.grid(row=2,column=0,columnspan=5)
        # self.lb2.grid(row = 1, column = 0)
 
 
         #Configuration
         self.win.configure(menu=self.menu)
-        self.win.columnconfigure(0, minsize=400)
+       
+        self.win.columnconfigure(0, minsize=500)
         self.win.columnconfigure(1, minsize=10)
-        self.win.columnconfigure(2, minsize=400)
+        self.win.columnconfigure(2, minsize=500)
         self.win.columnconfigure(3, minsize=10)
+        self.win.columnconfigure(4, minsize=500)
+       
+        self.win.rowconfigure(2, minsize=300)
+        self.win.rowconfigure(3,minsize =200)
 
-        self.win.columnconfigure(4, minsize=400)
-        self.win.columnconfigure(5, minsize=0)       
-        self.win.rowconfigure(0, minsize=0)
-        self.win.rowconfigure(1,minsize= 0)
-        self.win.rowconfigure(2, minsize=0)
-        self.win.rowconfigure(3,minsize =0)
-        self.win.rowconfigure(4, minsize=250)
-        self.win.rowconfigure(5, minsize=0)
-       # self.f1.configure(bg="#000066")
-        #self.f2.configure(bg="blue")
-       # self.f3.configure(bg="white") 
-        #self.f3_1.configure(bg="white")
-       # self.f3_2.configure(bg="white")
-       # self.f3_3.configure(bg="white")
-
-        self.b2.configure(command=self.REFRESH)
-        self.b3.configure(command=self.LOAD)
-        self.b7.configure(command=self.FOLDER)    
+        self.TREE[0].configure(command=self.REFRESH)
+        self.TREE[1].configure(command=self.LOAD)
+        self.TREE[2].configure(command=self.FOLDER)    
       
         self.l1.configure(width=35,fg="white",font="halvetica",background="blue")
         self.sb1.configure(command=self.lb1.yview)
@@ -129,82 +114,56 @@ class GUI(Frame):
         self.REFRESH()
      ##############################################
       #FUNCTOINS
-    def LOAD(self): # na razie zachowuje tylko nazwe pliku
+    def LOAD(self): 
       if self.lb1.curselection():
+        self.STATUS("PLOTTING")
         MGR = sf2r_manager( False  , True) #DEBUG = False API = True
       	self.file=str( self.filelist  [int(self.lb1.curselection()[0])] )
         plots = MGR.run_path(self.folder,self.file) # tu wywala TH1F'y
-  
-        canvas=[]
-        TOOLBAR=[]
-        canvas.append(plot_3d_2canvas(plots[0],self.win))
+        
+        if self.canvas:
+	   for i in range (3):
+               self.canvas[0][i].get_tk_widget().destroy()
+               self.TOOLBAR[i].destroy()  
+               pass                   
+
+        self.canvas=[]
+        self.TOOLBAR=[]
+        self.canvas.append(plot_3d_2canvas(plots[0],self.win))
 
         for i in range(3):
-            canvas[0][i].get_tk_widget().grid(row=2,column=2*i)
-            TOOLBAR.append(NavigationToolbar2TkAgg(canvas[0][i],self.win))
-            TOOLBAR[i].grid(row=3,column=2*i,sticky=NW)
+            self.canvas[0][i].get_tk_widget().grid(row=2,column=2*i)
+            self.TOOLBAR.append(NavigationToolbar2TkAgg(self.canvas[0][i],self.win))
+            self.TOOLBAR[i].grid(row=3,column=2*i,sticky=NW)
             
       	self.STATUS("FILE " +self.file+" LOADED") 
       
       else:
       	self.STATUS("FILE NOT SELECTED")	
+
     def REFRESH(self):
+
       self.lb1.delete(0, END)
       self.filelist=[f for f in listdir(self.folder) if path.isfile(path.join(self.folder,f)) if f != "gui.py"   if f != "gui.pyc"  ]
-      #print self.filelist
+
+      
+
       for i in self.filelist:
         self.lb1.insert(END,i)
+
+
     def FOLDER(self):
       tmp=self.folder
       self.folder=tkFileDialog.askdirectory()
       print self.folder
-      if self.folder !=():
+      if self.folder !="":
       	 self.STATUS("NEW DIRECTORY SELECTED")
       	 self.REFRESH()
       else:
       	self.folder = tmp	
 
-    def STATUS(self,string):# label print 
+    def STATUS(self,string):
         self.l1.configure(text=string)
-
-         #CANVAS PLOT
-    def D1(self):
-      if self.file==" ":
-      	self.STATUS("NO FILE LOADED")
-      else:
-      	self.canvas.delete("all")
-      	self.canvas.create_line(0, 0, 200, 100)#na razie cokolwiek
-      	self.STATUS("DETECTOR 1") 
-
-    def D2(self):
-      if self.file==" ":
-      	self.STATUS("NO FILE LOADED")
-      else:
-      
-        self.canvas.delete("all")
-        self.canvas.create_line(0, 400, 200, 120)#na razie cokolwiek
-        self.STATUS("DETECTOR 2")
-
-    def SP(self):
-      if self.file==" ":
-        self.STATUS("NO FILE LOADED")
-      else:
-        self.canvas.delete("all")
-        self.canvas.create_line(23, 0, 200, 400)#na razie cokolwiek
-        self.STATUS("SP")
-       
-    def CANVAS_SET(self):
-      if(self.f3_1 ):
-	self.f3_1.destroy()
-        self.f3_2.destroy()
-        self.f3_3.destroy()
-
-      self.f3_1=Frame(self.f3,height=600, width=500)
-      self.f3_2=Frame(self.f3,height=600, width=500)
-      self.f3_3=Frame(self.f3,height=600, width=500)    
-      self.f3_1.pack(side=LEFT)
-      self.f3_2.pack(side=LEFT)
-      self.f3_3.pack(side=LEFT)
 
     def EXIT(self):
       exit()    
